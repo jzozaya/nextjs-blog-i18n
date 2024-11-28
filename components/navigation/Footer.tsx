217b6f3d0200e5a4d7d8843cbc5847d9be97cbe4
Jorge Zozaya <jorge.zozaya.verdiguel@gmail.com>
2024-11-27 17:14:12 -0800

    updated avatar and site data

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -24,11 +24,13 @@ export default function Footer() {
     handleContactClick()
   }
 
+  
+
   return (
     <>
       <footer>
-        <div className="mt-16 flex flex-col items-center">
-          <div className="mb-3 flex space-x-4">
+        <div className="flex flex-col items-center mt-16">
+          <div className="flex mb-3 space-x-6">
             <div className="flex items-center">
               {siteMetadata.formspree === false ? (
                 <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
@@ -41,33 +43,33 @@ export default function Footer() {
             <div className="flex items-center">
               <SocialIcon kind="github" href={siteMetadata.github} size={6} />
             </div>
-            <div className="flex items-center">
+            <div className="items-center hidden">
               <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
             </div>
-            <div className="flex items-center">
+            <div className="items-center hidden">
               <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
             </div>
             <div className="flex items-center">
               <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
             </div>
-            <div className="flex items-center">
+            <div className="items-center hidden">
               <SocialIcon kind="x" href={siteMetadata.x} size={6} />
             </div>
             <div className="flex items-center">
               <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
             </div>
-            <div className="flex items-center">
+            <div className="items-center hidden">
               <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
             </div>
           </div>
-          <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div className="flex mb-6 space-x-2 text-sm text-gray-500 dark:text-gray-400">
             <div>{siteMetadata.author}</div>
             <div>{` • `}</div>
             <div>{`© ${new Date().getFullYear()}`}</div>
             <div>{` • `}</div>
             <Link href="/">{maintitle[locale]}</Link>
           </div>
-          <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <div className="hidden mb-8 text-sm text-gray-500 dark:text-gray-400">
             <Link href="https://github.com/PxlSyl/tailwind-nextjs-starter-blog-i18n">
               {t('theme')}
             </Link>
@@ -78,3 +80,108 @@ export default function Footer() {
     </>
   )
 }
+
+
+export function Example() {
+  return (
+    <div className="py-24 bg-gray-50 sm:py-32">
+      <div className="max-w-2xl px-6 mx-auto lg:max-w-7xl lg:px-8">
+        <h2 className="font-semibold text-center text-indigo-600 text-base/7">Deploy faster</h2>
+        <p className="max-w-lg mx-auto mt-2 text-4xl font-semibold tracking-tight text-center text-balance text-gray-950 sm:text-5xl">
+          Everything you need to deploy your app
+        </p>
+        <div className="grid gap-4 mt-10 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white lg:rounded-l-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Mobile friendly
+                </p>
+                <p className="max-w-lg mt-2 text-gray-600 text-sm/6 max-lg:text-center">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                  <img
+                    className="object-cover object-top size-full"
+                    src="https://tailwindui.com/plus/img/component-images/bento-03-mobile-friendly.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-1">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
+                <p className="max-w-lg mt-2 text-gray-600 text-sm/6 max-lg:text-center">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
+                </p>
+              </div>
+              <div className="flex items-center justify-center flex-1 px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                <img
+                  className="w-full max-lg:max-w-xs"
+                  src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+          </div>
+          <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+            <div className="absolute bg-white rounded-lg inset-px"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+              <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
+                <p className="max-w-lg mt-2 text-gray-600 text-sm/6 max-lg:text-center">
+                  Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.
+                </p>
+              </div>
+              <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
+                <img
+                  className="h-[min(152px,40cqw)] object-cover"
+                  src="https://tailwindui.com/plus/img/component-images/bento-03-security.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="absolute rounded-lg shadow pointer-events-none inset-px ring-1 ring-black/5"></div>
+          </div>
+          <div className="relative lg:row-span-2">
+            <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+              <div className="px-8 pt-8 pb-3 sm:px-10 sm:pb-0 sm:pt-10">
+                <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
+                  Powerful APIs
+                </p>
+                <p className="max-w-lg mt-2 text-gray-600 text-sm/6 max-lg:text-center">
+                  Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                </p>
+              </div>
+              <div className="relative min-h-[30rem] w-full grow">
+                <div className="absolute bottom-0 right-0 overflow-hidden bg-gray-900 shadow-2xl left-10 top-10 rounded-tl-xl">
+                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                    <div className="flex -mb-px font-medium text-gray-400 text-sm/6">
+                      <div className="px-4 py-2 text-white border-b border-r border-b-white/20 border-r-white/10 bg-white/5">
+                        NotificationSetting.jsx
+                      </div>
+                      <div className="px-4 py-2 border-r border-gray-600/10">App.jsx</div>
+                    </div>
+                  </div>
+                  <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
+                </div>
+              </div>
+            </div>
+            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
