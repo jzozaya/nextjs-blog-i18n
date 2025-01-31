bd91c5fd99e9c91f76a3b53c33cf3dbb7629a9b2
Jorge Zozaya <jorge.zozaya.verdiguel@gmail.com>
2025-01-30 21:55:21 -0800

    fix max width for post heading 1

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -6,7 +6,7 @@ interface PageTitleProps {
 
 export default function PageTitle({ children }: PageTitleProps) {
   return (
-    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 xl:max-w-3xl xl:mx-auto dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       {children}
     </h1>
   )
